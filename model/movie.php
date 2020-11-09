ef54f623a8b586e08a535f0db636da6898f866b2
shimizu6660 <b1205880@planet.kanazawa-it.ac.jp>
2020-11-10 01:04:21 +0900

    form実装

--- model/movie.php
+++ model/movie.php
@@ -44,7 +44,40 @@ function get_all_movie($db){
     return fetch_all_query($db, $wp_id);
 }
 
-function insert_movie($db, $movie_name, $movie_url, $movie_id, $character_id, $wp_id){
+  //moviesのレコード数取得
+  function table_col($db) {
+    $sql = "
+            SELECT 
+                COUNT(*)
+            FROM
+                movies
+            ";
+	
+    $statment = $db->query($sql);
+    return $statment->fetchColumn();
+}
+
+function regist_item($db, $name, $price, $stock, $status, $image){
+    $filename = get_upload_filename($image);
+    if(validate_item($name, $price, $stock, $filename, $status) === false){
+      return false;
+    }
+    return regist_item_transaction($db, $name, $price, $stock, $status, $image, $filename);
+  }
+  
+  function regist_item_transaction($db, $name, $price, $stock, $status, $image, $filename){
+    $db->beginTransaction();
+    if(insert_item($db, $name, $price, $stock, $filename, $status) 
+      && save_image($image, $filename)){
+      $db->commit();
+      return true;
+    }
+    $db->rollback();
+    return false;
+    
+}
+
+  function insert_movie($db, $movie_name, $movie_url, $movie_id, $character_id, $wp_id){
     $sql = "
       INSERT INTO
         items(
@@ -62,15 +95,60 @@ function insert_movie($db, $movie_name, $movie_url, $movie_id, $character_id, $w
     return execute_query($db, $sql, $array);
   }
 
-  //moviesのレコード数取得
-  function table_col($db) {
-    $sql = "
-            SELECT 
-                COUNT(*)
-            FROM
-                movies
-            ";
-	
-    $statment = $db->query($sql);
-    return $statment->fetchColumn();
-}
+  function validate_item($name, $price, $stock, $filename, $status){
+    $is_valid_item_name = is_valid_item_name($name);
+    $is_valid_item_price = is_valid_item_price($price);
+    $is_valid_item_stock = is_valid_item_stock($stock);
+    $is_valid_item_filename = is_valid_item_filename($filename);
+    $is_valid_item_status = is_valid_item_status($status);
+  
+    return $is_valid_item_name
+      && $is_valid_item_price
+      && $is_valid_item_stock
+      && $is_valid_item_filename
+      && $is_valid_item_status;
+  }
+  
+  function is_valid_item_name($name){
+    $is_valid = true;
+    if(is_valid_length($name, ITEM_NAME_LENGTH_MIN, ITEM_NAME_LENGTH_MAX) === false){
+      set_error('商品名は'. ITEM_NAME_LENGTH_MIN . '文字以上、' . ITEM_NAME_LENGTH_MAX . '文字以内にしてください。');
+      $is_valid = false;
+    }
+    return $is_valid;
+  }
+  
+  function is_valid_item_price($price){
+    $is_valid = true;
+    if(is_positive_integer($price) === false){
+      set_error('価格は0以上の整数で入力してください。');
+      $is_valid = false;
+    }
+    return $is_valid;
+  }
+  
+  function is_valid_item_stock($stock){
+    $is_valid = true;
+    if(is_positive_integer($stock) === false){
+      set_error('在庫数は0以上の整数で入力してください。');
+      $is_valid = false;
+    }
+    return $is_valid;
+  }
+  
+  function is_valid_item_filename($filename){
+    $is_valid = true;
+    if($filename === ''){
+      $is_valid = false;
+    }
+    return $is_valid;
+  }
+  
+  function is_valid_item_status($status){
+    $is_valid = true;
+    if(isset(PERMITTED_ITEM_STATUSES[$status]) === false){
+      $is_valid = false;
+    }
+    return $is_valid;
+  }
+  
